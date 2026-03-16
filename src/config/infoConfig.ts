355dad228c5f7ca197583f4a3831eb409bfa6a98
kinokotake <1369736948@qq.com>
2026-03-16 17:58:23 +0800

    update my info

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,12 +3,12 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'kinoko🍄'
+export const headline = 'ただいま。'
 export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
-export const githubUsername = 'derekhut'
+  "新しい始まりを。还没想好在这里写什么。"
+export const email = '1369736948@qq.com'
+export const githubUsername = 'kinokotake'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
